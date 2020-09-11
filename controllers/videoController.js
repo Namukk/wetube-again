3f7ca6d2500cd7ec8a6aa1e5329994bebad8af23
Namukk <skadnr625@gmail.com>
2020-09-12 00:14:41 +0300

    day 27 -comment and end

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
-routes;
+import Comment from "../models/Comment";
 
 // Home
 export const home = async (req, res) => {
@@ -64,7 +64,9 @@ export const videoDetail = async (req, res) => {
   } = req;
   // console.log(req.params.id); same with 43-45 req.params 하면 아이디 뜸.
   try {
-    const video = await Video.findById(id).populate("creator"); //populate() = 객체를 데려오는 함수, object ID에만 사용 가능
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments"); //populate() = 객체를 데려오는 함수, object ID에만 사용 가능
     res.render("videoDetail", { pageTitle: video.title, video });
     // video:video = video
   } catch (error) {
@@ -130,6 +132,29 @@ export const postRegisterView = async (req, res) => {
     video.views += 1;
     video.save();
     res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user,
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
   } catch (error) {
     res.status(400);
   } finally {
